b1fdb3fb263119536560aae8baed9c3bacfa0142
d4rkb4sh8 <145238010+d4rkb4sh8@users.noreply.github.com>
2025-04-11 08:18:51 -0400

    Update settings.backup.sh

--- settings.backup.sh
+++ settings.backup.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 #gnome settings
-#dconf dump / >$HOME/dotfiles/backups/gnome_settings.bak
+#dconf dump / >$HOME/dotfiles_kali/backups/gnome_settings.bak
 
 # XFCE settings
-cp -Trv $HOME/.config/xfce4 $HOME/dotfiles/backups/xfce4/
+cp -Trv $HOME/.config/xfce4 $HOME/dotfiles_kali/backups/xfce4/
 
 #apt_list
-apt list --installed | cut -d "/" -f1 | grep -v "Listing..." >$HOME/dotfiles/backups/apt_list.bak
+apt list --installed | cut -d "/" -f1 | grep -v "Listing..." >$HOME/dotfiles_kali/backups/apt_list.bak
 
 #flatpaks_list
-flatpak list --app --columns=application >$HOME/dotfiles/backups/flatpaks_list.bak
+flatpak list --app --columns=application >$HOME/dotfiles_kali/backups/flatpaks_list.bak
 
 # brew list
-brew list -1 >$HOME/dotfiles/backups/brew_list.bak
+brew list -1 >$HOME/dotfiles_kali/backups/brew_list.bak
 
 #snap list
-#snap list | awk 'NR>1{print $1}' >$HOME/dotfiles/backups/snap_list.bak
+#snap list | awk 'NR>1{print $1}' >$HOME/dotfiles_kali/backups/snap_list.bak
