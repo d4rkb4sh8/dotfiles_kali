d41837dfd2fb841cbefbbfe350bf481e38915a37
d4rkb4sh8 <d4rkb4sh8@gmail.com>
2025-04-30 11:10:31 -0400

    updated

--- backups/settings.backup.sh
+++ backups/settings.backup.sh
@@ -3,11 +3,8 @@
 #gnome settings
 dconf dump / >$HOME/dotfiles/backups/gnome_settings.bak
 
-# XFCE settings
-#cp -Trv $HOME/.config/xfce4 $HOME/dotfiles/backups/xfce4/
-
 #apt_list
-apt list --installed | cut -d "/" -f1 | grep -v "Listing..." >$HOME/dotfiles/backups/apt_list.bak
+dpkg-query -f '${Package}\n' -W >$HOME/dotfiles/backups/apt_list.bak
 
 #flatpaks_list
 flatpak list --app --columns=application >$HOME/dotfiles/backups/flatpaks_list.bak
