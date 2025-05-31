1805694a06eaa78d59b28ac9700623d5dc58b8cf
h4ck3r <d4rkb4sh8@gmail.com>
2025-05-31 10:00:05 -0400

    updated

--- kitty-zellij-emulation/kittens/status_bar/main.py
+++
@@ -1,7 +0,0 @@
-import datetime
-import os
-
-def main(args):
-    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    hostname = os.uname()[1]
-    print(f"[{hostname}] {now}")
--- kitty-zellij-emulation/kitty_zellij.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SESSION="main"
-
-if ! tmux has-session -t $SESSION 2>/dev/null; then
-  tmux new-session -d -s $SESSION
-fi
-
-kitty tmux attach -t $SESSION
